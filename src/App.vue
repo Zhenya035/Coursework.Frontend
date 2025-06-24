f16993296f59c5a6dbf52d21f3600c5d93024083
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-06-25 00:56:32 +0300

    add router and some libraries

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import { useRoute} from "vue-router";
+const route = useRoute();
 </script>
 
 <template>
-
+  <main :class="{'page-container': !['/login', '/registration'].includes(route.path)}">
+    <router-view/>
+  </main>
 </template>
 
 <style scoped>
